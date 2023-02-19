47ace06e60d2437a1d1391e0ec4be6043a2ffdad
yong liu <564447023@qq.com>
2023-02-20 00:45:04 +0800

    feat: implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let start = 0;
+    let end = haystack.length;
 
+    do {
+        const middle = Math.floor(start + (end - start) / 2);
+        if (haystack[middle] === needle) {
+            return true;
+        } else if (haystack[middle] < needle) {
+            start = middle + 1;
+        } else {
+            // haystack[middle] < needle
+            end = middle;
+        }
+    } while (start < end);
+
+    return false;
 }
