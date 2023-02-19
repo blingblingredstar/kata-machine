0b83fccf6802948f048cfb3433a31cf121ad0a81
yong liu <564447023@qq.com>
2023-02-20 00:55:31 +0800

    feat: implement two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpLength = Math.floor(Math.sqrt(breaks.length));
+    let i = 0;
 
+    while (breaks[i] === false) {
+        i += jumpLength;
+    }
+
+    for (let j = i - jumpLength + 1; j < i; j++) {
+        if (breaks[j] === true) {
+            return j;
+        }
+    }
+
+    return -1;
 }
