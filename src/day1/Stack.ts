27f32c8ac4e54219eaaae65b1643de440100f5fa
yong liu <564447023@qq.com>
2023-02-21 20:00:18 +0800

    feat: implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,34 @@
+interface Node<T> {
+    value: T;
+    prev: Node<T> | null;
+}
+
 export default class Stack<T> {
-    public length: number;
+    public length: number = 0;
 
-    
+    #top: Node<T> | null = null;
 
-    constructor() {
-    }
+    constructor() {}
 
     push(item: T): void {
-
-}
+        this.length++;
+        if (this.#top === null) {
+            this.#top = { value: item, prev: null };
+            return;
+        }
+        this.#top = { value: item, prev: this.#top };
+    }
     pop(): T | undefined {
-
-}
+        if (this.#top === null) {
+            return undefined;
+        }
+        this.length--;
+        const top = this.#top;
+        this.#top = top.prev;
+        top.prev = null;
+        return top.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.#top?.value;
+    }
 }
