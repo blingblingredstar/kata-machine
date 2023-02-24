07babeade72c0cb39b234f1b8e7bee07e8733bc3
Yong Liu <yong.liu@compass.com>
2023-02-24 12:28:38 +0800

    feat: implement bread first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,16 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = new Queue<typeof head | null | undefined>();
+
+    queue.enqueue(head);
+
+    while (queue.length > 0) {
+        const node = queue.deque();
+        if (!node) continue;
+        if (node.value === needle) return true;
+        queue.enqueue(node.left);
+        queue.enqueue(node.right);
+    }
+    return false;
 }
