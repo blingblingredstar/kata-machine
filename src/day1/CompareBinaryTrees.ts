4732f0e9b3c84a66138f9821148aa5a12588bafd
Yong Liu <yong.liu@compass.com>
2023-02-24 12:35:29 +0800

    feat: implement CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,11 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
-
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // Due to bread-first-search can't save the information of the level of the tree,
+    // we need to use depth-first-search and the order is doesn't matter here
+    if (a === null && b === null) return true;
+    if (a === null || b === null) return false;
+    if (a.value !== b.value) return false;
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
