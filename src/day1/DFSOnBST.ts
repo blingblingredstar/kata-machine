bccefd442bc8b5c2ebeff9a2edfe647d9ba6b04f
Yong Liu <yong.liu@compass.com>
2023-02-24 17:59:15 +0800

    feat: implement depth first search on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,11 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+    if (!head) return false;
+    if (head.value === needle) return true;
+    if (head.right && head.value < needle) {
+        return dfs(head.right, needle);
+    }
+    if (head.left && head.value >= needle) {
+        return dfs(head.left, needle);
+    }
+    return false;
 }
